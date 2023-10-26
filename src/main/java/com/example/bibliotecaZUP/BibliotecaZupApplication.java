dba05630d5eb57730e939e3f6da8c710f40be918
Yasmin Balbino <yasmin.balbino@zup.com.br>
2023-10-26 10:34:02 -0300

    iniciando projeto

---
+++ src/main/java/com/example/bibliotecaZUP/BibliotecaZupApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecaZUP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaZupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bibliotecaZUP/BibliotecaZupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecaZUP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
